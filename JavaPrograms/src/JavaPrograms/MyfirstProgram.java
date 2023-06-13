18e40ff1d80a849823d580894bdc1e2cf772da20
pandrakula-priyanka <pandrakulapriyanka33@gmail.com>
2023-06-13 16:48:35 +0530

    added java project

---
+++ JavaPrograms/src/JavaPrograms/MyfirstProgram.java
@@ -0,0 +1,8 @@
+package JavaPrograms;
+public class MyfirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+
+}
---
+++ JavaPrograms/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author PRIYA
+ *
+ */
+module JavaPrograms {
+}
